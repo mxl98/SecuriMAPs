04a7d8eef3e145f386da2870352827be7248455d
EmixamL <emixamlaferriere@gmail.com>
2024-12-18 15:31:08 -0500

    added light-dark CSS support and dark mapbox theme

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -10,6 +10,11 @@ import { environment } from '../../environments/environment';
   styleUrl: './map.component.scss'
 })
 export class MapComponent {
+  private mapboxUrls = {
+    'light': `https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/{z}/{x}/{y}?access_token=${ environment.mapboxToken }`,
+    'dark':  `https://api.mapbox.com/styles/v1/${ environment.mapboxUsername }/cm4ubpdkk00aa01qpgd9ahtfi/tiles/{z}/{x}/{y}?access_token=${ environment.mapboxToken }`
+  };
+  private darkMode: boolean = false;
   private map: any;
 
   private initMap(): void {
@@ -28,7 +33,7 @@ export class MapComponent {
     // osmTiles.addTo(this.map);
 
     // For MapBox-sourced tiles, comment if using another source because of pricing
-    const mapboxTiles = L.tileLayer(`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${ environment.mapboxToken }`, {
+    const mapboxTiles = L.tileLayer(this.darkMode? this.mapboxUrls.dark : this.mapboxUrls.light, {
       maxZoom: 19,
       minZoom: 12,
       attribution: '&copy; <a href="https://www.mapbox.com/">MapBox</a>',
@@ -39,7 +44,10 @@ export class MapComponent {
     mapboxTiles.addTo(this.map);
   }
 
-  constructor() {}
+  constructor() {
+    // i'd rather work using light mode, will uncomment when finished
+    //this.darkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+  }
 
   ngAfterViewInit(): void {
     this.initMap();
