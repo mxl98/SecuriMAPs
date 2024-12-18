d86b0fd2a4f04b14a8b437adc41a5a2db442fd95
EmixamL <emixamlaferriere@gmail.com>
2024-12-18 10:34:23 -0500

    integrated OpenStreetMap as tiles source

---
+++ frontend/src/app/map/map.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.scss'
+})
+export class MapComponent {
+  private map: any;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [45.5, -73.57],
+      zoom: 12
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      minZoom: 12,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+}
