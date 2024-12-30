297584a702edb44618e2f18503459822eb8f8d8c
EmixamL <emixamlaferriere@gmail.com>
2024-12-30 11:23:27 -0500

    declared sidebar service functions

--- frontend/src/app/sidebar.service.ts
+++ frontend/src/app/sidebar.service.ts
@@ -6,4 +6,27 @@ import { Injectable } from '@angular/core';
 export class SidebarService {
 
   constructor() { }
+
+  /**
+   * Changes the state of the sidebar based on its current state.
+   */
+  toggleSidebar(): void {
+
+  }
+
+  /**
+   * Expands the sidebar by applying an animation to its container and the drawer button and arrow.
+   * Changes the aria-expanded state to true.
+   */
+  expandSidebar(): void {
+
+  }
+
+  /**
+   * Retracts the sidebar by applying an animation to its container and the drawer button and arrow.
+   * Changes the aria-expanded state to false.
+   */
+  retractSidebar(): void {
+
+  }
 }
