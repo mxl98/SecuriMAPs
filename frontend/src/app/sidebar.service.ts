63a02724b144786133c1dd339c271d606aa237ed
EmixamL <emixamlaferriere@gmail.com>
2025-01-09 11:39:21 -0500

    animated sidebar container and drawer button

--- frontend/src/app/sidebar.service.ts
+++ frontend/src/app/sidebar.service.ts
@@ -4,27 +4,70 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SidebarService {
+  arrowMatrix = [
+    [17, 25, 7, 37],
+    [17, 47, 7, 35]
+  ];
 
   constructor() { }
 
   /**
-   * Changes the state of the sidebar based on its current state.
+   * Changes the state of the sidebar depending on its current state.
+   * @param isExpanded Tells if the sidebar is currently expanded.
    */
   toggleSidebar(isExpanded: boolean): void {
     isExpanded ? this.animateRetract() : this.animateExpand();
   }
 
   /**
-   * Applies an expanding animation to its container and the drawer button and arrow.
+   * Applies an expanding animation to its container and the drawer button.
    * Also moves buttons to the right.
    */
   animateExpand(): void {
+    let sidebar = document.getElementById('sidebar');
+    let drawer = document.getElementById('sidebar-drawer');
+    
+    sidebar?.classList.remove('sidebar-expand_animation');
+    sidebar?.classList.add('sidebar-retract_animation');
+
+    drawer?.classList.remove('drawer-expand_animation')
+    drawer?.classList.add('drawer-retract_animation');
   }
 
   /**
-   * Applies a retracting animation to its container and the drawer button and arrow.
+   * Applies a retracting animation to its container and the drawer button.
    * Also moves buttons to the left.
    */
   animateRetract(): void {
+    let sidebar = document.getElementById('sidebar');
+    let drawer = document.getElementById('sidebar-drawer');
+
+    sidebar?.classList.remove('sidebar-retract_animation');
+    sidebar?.classList.add('sidebar-expand_animation');
+
+    drawer?.classList.remove('drawer-retract_animation');
+    drawer?.classList.add('drawer-expand_animation')
+  }
+
+  /**
+   * Flips the draw button arrow by applying a reflection transformation on the arrow matrix.
+   * This swaps x1/x2 coordinates to point in the opposite direction.
+   * The flipped arrow matrix should look like this:
+   * [
+   *   [7, 25, 15, 37],
+   *   [7, 47, 17, 35]
+   * ]
+   * @param isExpanded Tells if the sidebar is currently expanded, then the arrow is pointing left.
+   */
+  flipArrow(isExpanded: boolean): number[][] {
+    this.arrowMatrix = this.arrowMatrix.map(([x1, y1, x2, y2]) => {
+      return [
+        x2,
+        y1,
+        x1,
+        y2
+      ];
+    });
+    return this.arrowMatrix;
   }
 }
--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
 import { SidebarService } from '../sidebar.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
   private _sidebarService: SidebarService;
   private isExpanded: boolean = false;
+  arrowMatrix: number[][];
 
   constructor(sidebarService: SidebarService) {
     this._sidebarService = sidebarService;
+    this.arrowMatrix = this._sidebarService.arrowMatrix;
   }
 
   toggleSidebar(): void {
+    this._sidebarService.toggleSidebar(this.isExpanded);
+
     let sidebar = document.getElementById('sidebar');
     this.isExpanded = sidebar?.getAttribute('aria-expanded') === 'true';
-    this._sidebarService.toggleSidebar(this.isExpanded);
-    sidebar?.setAttribute('aria-expanded', !this.isExpanded + "")
+    sidebar?.setAttribute('aria-expanded', !this.isExpanded + "");
+
+    this.arrowMatrix = this._sidebarService.flipArrow(this.isExpanded);
   }
 }
