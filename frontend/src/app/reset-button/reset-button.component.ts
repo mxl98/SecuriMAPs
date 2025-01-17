b8a6d2748f7ab303e074100eeedecea37b8354fa
EmixamL <emixamlaferriere@gmail.com>
2025-01-17 08:42:55 -0500

    added tooltip to reset button, needs new class for bottom-right tooltip arrow

--- frontend/src/app/reset-button/reset-button.component.ts
+++ frontend/src/app/reset-button/reset-button.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { MapService } from '../map.service';
+import { TooltipComponent } from '../tooltip/tooltip.component';
 
 @Component({
   selector: 'app-reset-button',
   standalone: true,
-  imports: [],
+  imports: [
+    TooltipComponent
+  ],
   templateUrl: './reset-button.component.html',
   styleUrl: './reset-button.component.scss'
 })
@@ -14,6 +17,7 @@ import { MapService } from '../map.service';
  */
 export class ResetButtonComponent {
   private _mapService: MapService;
+  tooltipMsg = "Center map";
 
   constructor(mapService: MapService) {
     this._mapService = mapService;
