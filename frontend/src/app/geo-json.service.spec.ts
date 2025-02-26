117d461939471e480d722369f39f5ca0f1797a5a
EmixamL <emixamlaferriere@gmail.com>
2025-02-26 11:20:04 -0500

    added tests

--- frontend/src/app/geo-json.service.spec.ts
+++ frontend/src/app/geo-json.service.spec.ts
@@ -2,9 +2,17 @@ import { TestBed } from '@angular/core/testing';
 
 import { GeoJSONService } from './geo-json.service';
 import { provideHttpClient } from '@angular/common/http';
+import L from 'leaflet';
 
 describe('GeoJSONService', () => {
   let service: GeoJSONService;
+  let testFiles = {
+    'LatLng':'assets/testing/geo-json-test.geojson'
+  };
+  let expectedLatLngArray: L.LatLng[] = [
+    new L.LatLng(45.455504503909204, -73.86161590004312),
+    new L.LatLng(45.486871369019063, -73.878548599748342)
+  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -18,4 +26,9 @@ describe('GeoJSONService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return a correct LatLng array', async () => {
+    let resultLatLngArray: L.LatLng[] = await service.getLatLngFromCoordinates(testFiles.LatLng);
+    expect(resultLatLngArray).toEqual(expectedLatLngArray);
+  })
 });
